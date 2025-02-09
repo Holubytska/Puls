a9bac9e0f1fc55f9cdf771b7bf95e9bef8d42219
Oksana Holubytska <oksanagolubickay@gmail.com>
2025-02-09 12:59:14 +0200

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+    speed: 1200,
+    // adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="icons/slider/left.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="icons/slider/right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__content")
+        .removeClass("catalog__content_active")
+        .eq($(this).index())
+        .addClass("catalog__content_active");
+    }
+  );
+});
