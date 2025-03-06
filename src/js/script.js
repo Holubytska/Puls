12523ed8db5a75e19ad7c8a8c8f1209ea4d40768
Oksana Holubytska <oksanagolubickay@gmail.com>
2025-03-06 04:53:16 +0200

    Changed files index.html, sass, js, smart.php

--- src/js/script.js
+++ src/js/script.js
@@ -32,4 +32,106 @@ $(document).ready(function () {
         .addClass("catalog__content_active");
     }
   );
+
+  // $(".catalog-item__link").each(function (i) {
+  //   $(this).on("click", function (e) {
+  //     e.preventDefault();
+  //     $(".catalog-item__content")
+  //       .eq(i)
+  //       .toggleClass("catalog-item__content_active");
+  //     $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+  //   });
+  // });
+
+  // $(".catalog-item__back").each(function (i) {
+  //   $(this).on("click", function (e) {
+  //     e.preventDefault();
+  //     $(".catalog-item__content")
+  //       .eq(i)
+  //       .toggleClass("catalog-item__content_active");
+  //     $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+  //   });
+  // });
+  //или оптимизированный вариант
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-item__content")
+          .eq(i)
+          .toggleClass("catalog-item__content_active");
+        $(".catalog-item__list").eq(i).toggleClass("catalog-item__list_active");
+      });
+    });
+  }
+
+  toggleSlide(".catalog-item__link");
+  toggleSlide(".catalog-item__back");
+
+  //Modal
+
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+  });
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
+
+  function valideForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!"),
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты",
+        },
+      },
+    });
+  }
+
+  valideForms("#consultation-form");
+  valideForms("#consultation form");
+  valideForms("#order form");
+
+  $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+  $("form").submit(function (e) {
+    e.preventDefault();
+    if (!$(this).valid()) {
+      return;
+    }
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn("slow");
+
+      $("form").trigger("reset");
+    });
+    return false;
+  });
 });
